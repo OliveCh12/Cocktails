3c3626276690d96d62499c93216e9e6fd5f55901
Olivier <olivierchemla@gmail.com>
2019-05-13 02:39:18 +0200

    Updating Component

--- src/components/Recepe.js
+++ src/components/Recepe.js
@@ -1,6 +1,13 @@
 import React, { useState, useEffect } from "react";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSyncAlt, faListUl } from "@fortawesome/free-solid-svg-icons";
+
 import Recepes from "../data/recepes.json";
+
+const show = {
+  display: "block"
+};
 
 const Recepe = () => {
   const [seconds, setSeconds] = useState(0);
@@ -10,30 +17,47 @@ const Recepe = () => {
     ingredients: []
   });
 
-    useEffect(() => {
-      const interval = setInterval(() => {
-        setSeconds(seconds => seconds + 1);
-      }, 1000);
-      return () => clearInterval(interval);
-    }, []);
+  //   useEffect(() => {
+  //     const interval = setInterval(() => {
+  //       setSeconds(seconds => seconds + 1);
+  //     }, 1000);
+  //     return () => clearInterval(interval);
+  //   }, []);
 
   const randomRecepe = () => {
     const item =
       Recepes.content[Math.floor(Math.random() * Recepes.content.length)];
-    setRecepe({ name: item.name, type: item.type, ingredients: item.ingredients });
+    setRecepe({
+      name: item.name,
+      type: item.type,
+      ingredients: item.ingredients
+    });
   };
-
   return (
     <div className="Recepe">
       <header className="App-header">
-        <h1>{recepe.name}</h1>
-        <small>{`(${recepe.type})`}</small>
-        <ul>
+        {recepe.name === "" ? (
+          <div style={{ textAlign: "center" }}>
+            <h1 className="pulse">Pablo Cocktails</h1>
+            <small>Nombre de Cocktails : {Recepes.content.length}</small>
+          </div>
+        ) : (
+          <h1>{recepe.name}</h1>
+        )}
+        <small className={recepe.type.replace(/\s/g, "")}>{recepe.type}</small>
+        <ul style={show}>
           {recepe.ingredients.map(item => (
             <li key={item}>{item}</li>
           ))}
         </ul>
-        <button onClick={randomRecepe} className="btn-footer">Générer un Cocktail</button>
+        <div className="btn-group">
+          <button onClick={randomRecepe} className="btn-footer">
+            <FontAwesomeIcon icon={faListUl} size="2x" />
+          </button>
+          <button onClick={randomRecepe} className="btn-footer">
+            <FontAwesomeIcon icon={faSyncAlt} size="2x" />
+          </button>
+        </div>
       </header>
     </div>
   );
